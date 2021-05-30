e062cb7c337d7ba14b21a851e327c3442926771e
capsule <valentin.rudloff.perso@gmail.com>
2021-05-30 23:48:20 +0200

    add systemctl

--- devices_driver/imu/install.py
+++ devices_driver/imu/install.py
@@ -6,6 +6,8 @@ print("Configuring imu_measure app...")
 path_to_app = "/etc/capsule/imu_measure"
 path_to_log = "/var/log/capsule/imu_measure"
 path_to_conf = "/etc/capsule/imu_measure/config.yaml"
+path_to_conf = "/etc/capsule/imu_measure/config.yaml"
+path_to_services = "/etc/systemd/system/imu_measure.service"
 if not os.path.exists(path_to_app):
     print("Create folder", path_to_app)
     os.makedirs(path_to_app, 0o775)
@@ -21,5 +23,8 @@ if not os.path.exists(path_to_conf):
     shutil.copy2(os.path.join(os.path.dirname(__file__), "default_config.yaml"), path_to_conf)
     os.chown(path_to_conf, 1000, 0) # Rudloff id and group Root
     os.chmod(path_to_conf, 0o775) # Give all read access but Rudloff write access
-else:
-    print("IMU measure configuration file already installed")
+if not os.path.exists(path_to_services):
+    shutil.copy2(os.path.join(os.path.dirname(__file__), "imu.service"), path_to_services)
+    os.chmod(path_to_services, 0o775) # Give all read access but Rudloff write access
+    os.system("systemctl daemon-reload")
+    os.system("systemctl enable imu_measure.service")
