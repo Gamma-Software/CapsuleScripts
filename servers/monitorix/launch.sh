a63a105dbaff79f453f3eadff237145cf65d040e
capsule <valentin.rudloff.perso@gmail.com>
2021-06-07 23:48:56 +0200

    add servers

---
+++ servers/monitorix/launch.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+if [ ! -z "$MONITORIX_PORT" ]; then
+  MONITORIX_CONF="/etc/monitorix/monitorix.conf"
+  sed -i -e "s/port = 8080/port = $MONITORIX_PORT/g" $MONITORIX_CONF
+  sed -i -e "s/:8080/:$MONITORIX_PORT/g" $MONITORIX_CONF
+fi
