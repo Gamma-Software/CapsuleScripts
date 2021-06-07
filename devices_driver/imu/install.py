a5641ea0fb92937abccd551497948da76b93b9bd
capsule <valentin.rudloff.perso@gmail.com>
2021-06-07 22:24:31 +0200

    remane imu.services to imu_measure.services

--- devices_driver/imu/install.py
+++ devices_driver/imu/install.py
@@ -24,7 +24,7 @@ if not os.path.exists(path_to_conf):
     os.chown(path_to_conf, 1000, 0) # Rudloff id and group Root
     os.chmod(path_to_conf, 0o775) # Give all read access but Rudloff write access
 if not os.path.exists(path_to_services):
-    shutil.copy2(os.path.join(os.path.dirname(__file__), "imu.service"), path_to_services)
+    shutil.copy2(os.path.join(os.path.dirname(__file__), "imu_measure.service"), path_to_services)
     os.chmod(path_to_services, 0o775) # Give all read access but Rudloff write access
     os.system("systemctl daemon-reload")
     os.system("systemctl enable imu_measure.service")
