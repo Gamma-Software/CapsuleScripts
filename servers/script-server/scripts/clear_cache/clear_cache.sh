25b0ae83933397b604b2702f24208df5b2217b63
capsule <valentin.rudloff.perso@gmail.com>
2021-08-09 22:58:42 +0000

    update script server scripts

--- servers/script-server/scripts/clear_cache/clear_cache.sh
+++ servers/script-server/scripts/clear_cache/clear_cache.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # Note, we are using "echo 3", but it is not recommended in production instead use "echo 1"
 echo "echo 1 > /proc/sys/vm/drop_caches"
+echo 1 > /proc/sys/vm/drop_caches
---
+++ servers/script-server/scripts/manage_users.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/bash
+htpasswd -b /home/rudloff/.user-htpasswd/.htpasswd $SCRIPT_USERS $USER_PASS
